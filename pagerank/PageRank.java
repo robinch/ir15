5f7db34f49d125a8f85efa6ea148b9ca82851146
Robin Chowdhury <robinch88@gmail.com>
2015-03-03 15:52:34 +0100

    done with 2.4

--- pagerank/PageRank.java
+++ pagerank/PageRank.java
@@ -15,7 +15,7 @@ public class PageRank{
      *   Maximal number of documents. We're assuming here that we
      *   don't have more docs than we can keep in main memory.
      */
-    final static int MAX_NUMBER_OF_DOCS = 100; // org value 2000000
+    final static int MAX_NUMBER_OF_DOCS = 200000; // org value 2000000
 
     /**
      *   Mapping from document names to document numbers.
@@ -54,19 +54,19 @@ public class PageRank{
      *   The probability that the surfer will be bored, stop
      *   following links, and take a random jump somewhere.
      */
-    final static double BORED = 0.15;
+    final static double BORED = 0.15; // org valyue 0.15
 
     /**
      *   Convergence criterion: Transition probabilities do not 
      *   change more that EPSILON from one iteration to another.
      */
-    final static double EPSILON = 0.0001;   // org value 0.0001
+    final static double EPSILON = 0.00001;   // org value 0.0001
 
     /**
      *   Never do more than this number of iterations regardless
      *   of whether the transistion probabilities converge or not.
      */
-    final static int MAX_NUMBER_OF_ITERATIONS = 10;   //org value 1000
+    final static int MAX_NUMBER_OF_ITERATIONS = 1000;   //org value 1000
 
     
     /* --------------------------------------------- */
@@ -167,29 +167,75 @@ return fileIndex;
         double[] xs = new double[numberOfDocs];
         double diffSum = 0;
         xs[0] = 1;
-
+        double sum = 0;
         for (int i = 0; i < MAX_NUMBER_OF_ITERATIONS; i++){
             diffSum = 0;
+            
+            x = xs;
+            xs = new double[numberOfDocs];
+            sum = 0;
             for (int j = 0; j < numberOfDocs; j++ ){
-                diffSum += Math.abs(xs[i] - x[i]);
+             
+                sum += x[j];    
+                // is a sink
+                if(out[j] == 0){
+                    for(int k = 0; k < xs.length; k++){
+                        if(k != j){
+                            // adds this value to all but it's own index
+                            xs[k] += x[j]*(1-BORED)/(numberOfDocs-1);    
+                        }   
+                    }
+                } else { // i not sink
+                    for(Integer l: link.get(j).keySet()){
+                        // System.out.format("j = %d keys: %d%n", j, l);
+                        xs[l] += x[j]*(1-BORED)/out[j];
+                    }
+                }
+            }
+            for (int j = 0; j < numberOfDocs; j++ ){
+                xs[j] += BORED/numberOfDocs;
+                diffSum = Math.max(diffSum, Math.abs(xs[j] - x[j]));
+            }
+            
 
-            }
             if(diffSum <= EPSILON){
-                printResult(xs);
+                System.out.println("EPSILON!!!");
+                printResult(xs, true);
+                return;
             }
+            System.out.println("iteration: "+ i);
         }
-        printStuff();
-        
+        printResult(xs, true);
+        // printStuff();
+
     }
 
 
-    private void printResult(double[] x){ 
-        // class Pair implements Comparable{
-        //     String name;
-        //     double score;
+    private void printResult(double[] x, boolean sort){ 
+        class Pair implements Comparable<Pair>{
+            String name;
+            double score;
+
+            Pair(String name, double score){
+                this.name = name;
+                this.score = score;
+            }
+
+            public int compareTo( Pair other ) {
+                return Double.compare( other.score, score );
+            }
+        }
 
 
-        // }
+        List<Pair> list = new ArrayList<Pair>(x.length);
+        for(int i = 0; i < x.length; i++){
+            list.add(new Pair(docName[i], x[i]));
+        }
+        if(sort) Collections.sort(list);
+
+        for(int i = 0; i < 60; i++){
+            System.out.format("%d: %s %f%n", i+1, list.get(i).name, list.get(i).score);
+        }
         // to print them in order I need to to either
         // add them in a structure that keeps track of the 
         // original position in the array or 
@@ -198,10 +244,7 @@ return fileIndex;
         // sturcture has the O(nLogn)
         // iterate O(50n)
 
-        // Arrays.sort(x);
-        // for (int i = 0; int < 50; i++ ){
-        //     System.out.format("%d: ")
-        // }
+
 
     }
 
@@ -209,9 +252,9 @@ return fileIndex;
     private void printStuff(){
 
         for(Integer i : link.keySet()){
-            System.out.println("From: " +docName[i]);
+            System.out.println("From: " +i);
             for(Integer j : link.get(i).keySet()){
-                System.out.println("\tTo: " +docName[j]);
+                System.out.println("\tTo: " +j);
             }    
             System.out.println("Nr of links: " + out[i]);
         }

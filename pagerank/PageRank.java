13a5c47f5b3a0d035000a62ac50590e8fd66bcc2
Robin Chowdhury <robinch88@gmail.com>
2015-03-03 16:32:16 +0100

    cleaned the code a bit

--- pagerank/PageRank.java
+++ pagerank/PageRank.java
@@ -73,12 +73,12 @@ public class PageRank{
 
 
     public PageRank( String filename ) {
-     int noOfDocs = readDocs( filename );
-     computePagerank( noOfDocs );
- }
+       int noOfDocs = readDocs( filename );
+       computePagerank( noOfDocs );
+   }
 
 
- /* --------------------------------------------- */
+   /* --------------------------------------------- */
 
 
     /**
@@ -91,61 +91,61 @@ public class PageRank{
      *   @return the number of documents read.
      */
     int readDocs( String filename ) {
-     int fileIndex = 0;
-     try {
-         System.err.print( "Reading file... " );
-         BufferedReader in = new BufferedReader( new FileReader( filename ));
-         String line;
-         while ((line = in.readLine()) != null && fileIndex<MAX_NUMBER_OF_DOCS ) {
-          int index = line.indexOf( ";" );
-          String title = line.substring( 0, index );
-          Integer fromdoc = docNumber.get( title );
+       int fileIndex = 0;
+       try {
+           System.err.print( "Reading file... " );
+           BufferedReader in = new BufferedReader( new FileReader( filename ));
+           String line;
+           while ((line = in.readLine()) != null && fileIndex<MAX_NUMBER_OF_DOCS ) {
+              int index = line.indexOf( ";" );
+              String title = line.substring( 0, index );
+              Integer fromdoc = docNumber.get( title );
 		//  Have we seen this document before?
-          if ( fromdoc == null ) {	
+              if ( fromdoc == null ) {	
 		    // This is a previously unseen doc, so add it to the table.
-              fromdoc = fileIndex++;
-              docNumber.put( title, fromdoc );
-              docName[fromdoc] = title;
-          }
+                  fromdoc = fileIndex++;
+                  docNumber.put( title, fromdoc );
+                  docName[fromdoc] = title;
+              }
 		// Check all outlinks.
-          StringTokenizer tok = new StringTokenizer( line.substring(index+1), "," );
-          while ( tok.hasMoreTokens() && fileIndex<MAX_NUMBER_OF_DOCS ) {
-              String otherTitle = tok.nextToken();
-              Integer otherDoc = docNumber.get( otherTitle );
-              if ( otherDoc == null ) {
+              StringTokenizer tok = new StringTokenizer( line.substring(index+1), "," );
+              while ( tok.hasMoreTokens() && fileIndex<MAX_NUMBER_OF_DOCS ) {
+                  String otherTitle = tok.nextToken();
+                  Integer otherDoc = docNumber.get( otherTitle );
+                  if ( otherDoc == null ) {
 			// This is a previousy unseen doc, so add it to the table.
-               otherDoc = fileIndex++;
-               docNumber.put( otherTitle, otherDoc );
-               docName[otherDoc] = otherTitle;
-           }
+                     otherDoc = fileIndex++;
+                     docNumber.put( otherTitle, otherDoc );
+                     docName[otherDoc] = otherTitle;
+                 }
 		    // Set the probability to 0 for now, to indicate that there is
 		    // a link from fromdoc to otherDoc.
-           if ( link.get(fromdoc) == null ) {
-               link.put(fromdoc, new Hashtable<Integer,Boolean>());
-           }
-           if ( link.get(fromdoc).get(otherDoc) == null ) {
-               link.get(fromdoc).put( otherDoc, true );
-               out[fromdoc]++;
-           }
-       }
-   }
-   if ( fileIndex >= MAX_NUMBER_OF_DOCS ) {
-      System.err.print( "stopped reading since documents table is full. " );
-  }
-  else {
-      System.err.print( "done. " );
-  }
+                 if ( link.get(fromdoc) == null ) {
+                     link.put(fromdoc, new Hashtable<Integer,Boolean>());
+                 }
+                 if ( link.get(fromdoc).get(otherDoc) == null ) {
+                     link.get(fromdoc).put( otherDoc, true );
+                     out[fromdoc]++;
+                 }
+             }
+         }
+         if ( fileIndex >= MAX_NUMBER_OF_DOCS ) {
+          System.err.print( "stopped reading since documents table is full. " );
+      }
+      else {
+          System.err.print( "done. " );
+      }
 	    // Compute the number of sinks.
-  for ( int i=0; i<fileIndex; i++ ) {
-      if ( out[i] == 0 )
-          numberOfSinks++;
+      for ( int i=0; i<fileIndex; i++ ) {
+          if ( out[i] == 0 )
+              numberOfSinks++;
+      }
   }
-}
-catch ( FileNotFoundException e ) {
- System.err.println( "File " + filename + " not found!" );
+  catch ( FileNotFoundException e ) {
+   System.err.println( "File " + filename + " not found!" );
 }
 catch ( IOException e ) {
- System.err.println( "Error reading file " + filename );
+   System.err.println( "Error reading file " + filename );
 }
 System.err.println( "Read " + fileIndex + " number of documents" );
 return fileIndex;
@@ -166,17 +166,15 @@ return fileIndex;
         double[] x = new double[numberOfDocs];
         double[] xs = new double[numberOfDocs];
         double diffSum = 0;
+        double jPart = BORED/numberOfDocs;;
         xs[0] = 1;
-        double sum = 0;
         for (int i = 0; i < MAX_NUMBER_OF_ITERATIONS; i++){
             diffSum = 0;
-            
             x = xs;
             xs = new double[numberOfDocs];
-            sum = 0;
+
             for (int j = 0; j < numberOfDocs; j++ ){
-             
-                sum += x[j];    
+                // adds the cP part
                 // is a sink
                 if(out[j] == 0){
                     for(int k = 0; k < xs.length; k++){
@@ -185,94 +183,64 @@ return fileIndex;
                             xs[k] += x[j]*(1-BORED)/(numberOfDocs-1);    
                         }   
                     }
-                } else { // i not sink
+                } else { // is not a sink
+                    double cPart = (1-BORED)/out[j];
                     for(Integer l: link.get(j).keySet()){
-                        // System.out.format("j = %d keys: %d%n", j, l);
-                        xs[l] += x[j]*(1-BORED)/out[j];
+                        xs[l] += x[j]*cPart;
                     }
                 }
             }
+
+            // adds the (1-c)J part
             for (int j = 0; j < numberOfDocs; j++ ){
-                xs[j] += BORED/numberOfDocs;
+                xs[j] += jPart;
                 diffSum = Math.max(diffSum, Math.abs(xs[j] - x[j]));
             }
             
-
+            // Checks if the largest difference is smaller than epsilon
             if(diffSum <= EPSILON){
                 System.out.println("EPSILON!!!");
-                printResult(xs, true);
-                return;
+                break;
             }
             System.out.println("iteration: "+ i);
         }
-        printResult(xs, true);
-        // printStuff();
-
+        printResult(xs);
     }
 
 
-    private void printResult(double[] x, boolean sort){ 
+    private void printResult(double[] x){ 
         class Pair implements Comparable<Pair>{
             String name;
             double score;
-
             Pair(String name, double score){
                 this.name = name;
                 this.score = score;
             }
-
             public int compareTo( Pair other ) {
                 return Double.compare( other.score, score );
             }
         }
 
-
         List<Pair> list = new ArrayList<Pair>(x.length);
         for(int i = 0; i < x.length; i++){
             list.add(new Pair(docName[i], x[i]));
         }
-        if(sort) Collections.sort(list);
+        
+        Collections.sort(list);
 
-        for(int i = 0; i < 60; i++){
-            System.out.format("%d: %s %f%n", i+1, list.get(i).name, list.get(i).score);
-        }
-        // to print them in order I need to to either
-        // add them in a structure that keeps track of the 
-        // original position in the array or 
-        // iterate thorugh the array 50 times to find and 
-        // print them one by one
-        // sturcture has the O(nLogn)
-        // iterate O(50n)
-
-
-
-    }
-
-
-    private void printStuff(){
-
-        for(Integer i : link.keySet()){
-            System.out.println("From: " +i);
-            for(Integer j : link.get(i).keySet()){
-                System.out.println("\tTo: " +j);
-            }    
-            System.out.println("Nr of links: " + out[i]);
-        }
-
-        for (int i = 0; i < docName.length; i++){
-            System.out.format(" docName[%d] = %s : %d\n", i, docName[i], docNumber.get(docName[i] ));
+        for(int i = 0; i < 50; i++){
+            System.out.format("%d: %s %.8f%n", i+1, list.get(i).name, list.get(i).score);
         }
     }
-
 
     /* --------------------------------------------- */
 
     public static void main( String[] args ) {
-     if ( args.length != 1 ) {
-         System.err.println( "Please give the name of the link file" );
-     }
-     else {
-         new PageRank( args[0] );
-     }
- }
+       if ( args.length != 1 ) {
+           System.err.println( "Please give the name of the link file" );
+       }
+       else {
+           new PageRank( args[0] );
+       }
+   }
 }
